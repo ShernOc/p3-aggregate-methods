8d98c55a5dc0595e525d1a45baafd1800a6ceb0b
ShernOC <sherlynea1994@hotmail.com>
2024-12-08 11:53:10 +0300

    aggregated

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -3,6 +3,7 @@ class Student:
     def __init__(self, name):
         self.name = name
         self._enrollments = []
+        self._grades = {}
 
     def enroll(self, course):
         if isinstance(course, Course):
@@ -11,15 +12,31 @@ class Student:
             course.add_enrollment(enrollment)
         else:
             raise TypeError("course must be an instance of Course")
+            
 
     def get_enrollments(self):
         return self._enrollments.copy()
+    
+    def course_count(self):
+        return len(self._enrollments)
+    
+    
+    def aggregate_average_grade(self):
+      # lets assume the grades are stored in a protected attribute called _grades. 
+      total_grades = sum(self._grades.values())
+      num_courses = len(self._grades)
+      average_grade = total_grades / num_courses
 
+      return average_grade
+    
+
+    
 class Course:
     def __init__(self, title):
 
         self.title = title
         self._enrollments = []
+        
 
     def add_enrollment(self, enrollment):
         if isinstance(enrollment, Enrollment):
@@ -29,6 +46,7 @@ class Course:
 
     def get_enrollments(self):
         return self._enrollments.copy()
+    
 
 
 class Enrollment:
@@ -42,6 +60,15 @@ class Enrollment:
             type(self).all.append(self)
         else:
             raise TypeError("Invalid types for student and/or course")
+        
 
     def get_enrollment_date(self):
         return self._enrollment_date
+    
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1 #(key;value) ; date,0
+        return enrollment_count
